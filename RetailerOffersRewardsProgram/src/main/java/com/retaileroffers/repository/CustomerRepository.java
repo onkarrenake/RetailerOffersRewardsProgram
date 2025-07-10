b281eeac71b9d1978f811ef99693ad836b13afeb
Onkar Renake <64122726+onkarrenake@users.noreply.github.com>
2025-07-10 14:15:54 +0530

    Query
    Added Query to fetch all customers from last 3 monts

--- RetailerOffersRewardsProgram/src/main/java/com/retaileroffers/repository/CustomerRepository.java
+++ RetailerOffersRewardsProgram/src/main/java/com/retaileroffers/repository/CustomerRepository.java
@@ -1,6 +1,11 @@
 package com.retaileroffers.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.retaileroffers.entity.Customer;
 
@@ -10,5 +15,12 @@ import com.retaileroffers.entity.Customer;
  */
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+	
+    /**
+     * This query joins the Customer entity with its associated transactions (tranx)
+     * and filters based on the transaction date.
+     */
+	@Query("SELECT DISTINCT c FROM Customer c JOIN c.tranx t WHERE t.date >= :startDate")
+	List<Customer> getLast3MonthsPointsSummary(@Param("startDate") LocalDate startDate);
+	
 }
